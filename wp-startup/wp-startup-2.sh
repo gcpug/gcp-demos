6b65df82bd2c717b42037e967fb400541d393aeb
ayatoshi <ayatoshi@yoshidumi.co.jp>
2015-05-06 16:43:38 +0900

    Update wp-startup-2.sh
    html削った。

--- wp-startup/wp-startup-2.sh
+++ wp-startup/wp-startup-2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 cd
 #以下、最新のWPのバージョンを指定する。
-curl -L -O http://ja.wordpress.org/wordpress-4.1.1-ja.zip
+curl -L -O http://ja.wordpress.org/wordpress-4.2.1-ja.zip
 unzip wordpress-*-ja.zip
-sudo mv wordpress/* /var/www/html
-sudo mv /var/www/html/index.html /var/www/html/index.html.bk
-sudo chgrp -R www-data /var/www/html
-sudo chmod -R g+w /var/www/html
+sudo mv wordpress/* /var/www
+sudo mv /var/www/index.html /var/www/index.html.bk
+sudo chgrp -R www-data /var/www
+sudo chmod -R g+w /var/www
 
